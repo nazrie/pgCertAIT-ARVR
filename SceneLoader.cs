05001ed64dda8ec834fb653ae86fe4c548df9d44
NazrieAS <nazrie.othman@gmail.com>
2021-02-10 20:51:41 +0000

    Add files via upload
    Scene loader - changing scene

---
+++ SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton(buttonName: "Button-3-breathing"))
+        {
+            ChangeScene();
+        }
+
+        if (Input.GetButton("Button-TensionRelease"))
+        {
+            ChangeScene2();
+        }
+
+    }
+
+    public void ChangeScene()
+        {
+            SceneManager.LoadScene("GreenNature");
+        }
+
+
+    public void ChangeScene2()
+        {
+           SceneManager.LoadScene("OpenSpaceSea");
+        } 
+
+    
+}
